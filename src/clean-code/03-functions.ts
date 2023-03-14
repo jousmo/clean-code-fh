8015afdc27df6b15af27baf0688db365c8041c96
jose.uscanga <jousmo@outlook.com>
2023-03-14 10:40:16 -0600

    feat: good names example 03 name functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,34 +1,34 @@
 (() => {
-  // función para obtener información de una película por Id
-  function getAllMovies( movieId: string ) {
-    console.log({ movieId });
-  }
-
-  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors( id: string ) {
+  function getMovieById(id: string) {
     console.log({ id });
   }
 
-  // funcion para obtener el bio del actor por el id
-  function getUsuario( ActorId: string ) {
-    console.log({ ActorId });
+  function getMovieCastById(id: string) {
+    console.log({ id });
   }
 
-  // Crear una película
-  function movie(title: string, description: string, rating: number, cast: string[] ) {
+  function getActorBioById(id: string) {
+    console.log({ id });
+  }
+
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
+  }
+
+  function createMovie({ title, description, rating, cast }: Movie) {
     console.log({ title, description, rating, cast });
   }
 
-  // Crea un nuevo actor
-  function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+  function checkFullName (fullName: string): boolean {
+    console.log({ fullName })
+    return true
+  }
 
-    // tarea asincrona para verificar nombre
-    // ..
-    // ..
-    if ( fullName === 'fernando' ) return false;
-
+  function createActor(fullName: string, birthdate: Date): boolean {
     console.log('Crear actor');
-    return true;
-
+    return checkFullName(fullName)
   }
 })();
