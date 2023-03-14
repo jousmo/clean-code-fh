6bb889efbd30e53b4f692e657cc92ed83ea5eb57
jose.uscanga <jousmo@outlook.com>
2023-03-14 12:44:44 -0600

    feat: bad example 06, no apply single responsibility principle

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,11 +8,25 @@ class Product {
 
   ) {}
 
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case 'string':
+          if (this[key].length <= 0) throw Error(`${key} is empty`);
+          break;
+        case 'number':
+          if (this[key] <= 0) throw Error(`${key} is zero`);
+          break;
+        default:
+          throw Error(`${typeof this[key]} is not supported`);
+      }
+    }
+    return true
+  }
+
   toString() {
-    // No Dry
-    if (this.name.length <= 0) throw Error('Name is empty')
-    if (this.price <= 0) throw Error('Price is zero')
-    if (this.size.length <= 0) throw Error('Size is empty')
+    if (!this.isProductReady()) return
+
     return `${this.name} ${this.price}, ${this.size}`
   }
 }
